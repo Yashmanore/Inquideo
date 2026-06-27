7b371158bc644e6528479435d4515e202cc061b1
Yashmanore <yashmanore12@gmail.com>
2026-06-28 00:55:25 +0530

    first commit

---
+++ test.js
@@ -0,0 +1,19 @@
+import { GoogleGenAI } from "@google/genai";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const ai = new GoogleGenAI({
+    apiKey: process.env.GEMINI_API_KEY,
+});
+
+async function main() {
+    const response = await ai.models.embedContent({
+        model: "gemini-embedding-001",
+        contents: "What is the meaning of life?",
+    });
+
+    console.log(response);
+}
+
+main();
